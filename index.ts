6cef5ed657d8a90b98b1d0324f2481b8898b660c
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-05 19:33:34 +0700

    7.3. Lấy ra chi tiết công việc

--- index.ts
+++ index.ts
@@ -10,12 +10,23 @@ import Task from "./models/task.model";
 const app: Express = express();
 const port: (number | string) = `${process.env.PORT}` || 3000;
 
-app.get("/tasks", async (req: Request, res: Response) => {
+app.get("/tasks", async (req: Request, res: Response): Promise<void> => {
   const tasks = await Task.find({
     deleted: false
   });
-  
+
   res.json(tasks);
+});
+
+app.get("/tasks/detail/:id", async (req: Request, res: Response): Promise<void> => {
+  const id: string = req.params.id;
+
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false
+  });
+  
+  res.json(task);
 });
 
 app.listen(port, () => {
