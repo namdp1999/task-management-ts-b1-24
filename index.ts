47c86511fa7417cf20f32ee88bc52673e73b1369
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-06-05 19:28:36 +0700

    7.2. Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("OK123");
+import express, { Express, Request, Response } from "express";
+import env from "dotenv";
+env.config();
+
+import { connect } from "./config/database";
+connect();
+
+import Task from "./models/task.model";
+
+const app: Express = express();
+const port: (number | string) = `${process.env.PORT}` || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false
+  });
+  
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
